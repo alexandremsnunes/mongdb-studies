ceb3e714a69779e452671d884f8644d1d7a28d61
alexandremsnunes <msn.alexandre@gmail.com>
2021-04-16 16:37:43 -0300

    Started project

---
+++ mongodb-project/src/test/java/com/alexandrenunes/mongodbproject/MongodbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrenunes.mongodbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
